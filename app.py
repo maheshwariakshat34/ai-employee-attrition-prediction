88cc66c3c1cc1ba28e20136363f6ae02a8b8a8de
maheshwariakshat34 <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-03-21 00:01:46 +0530

    import the new auth blueprint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from routes.prediction import prediction_bp
+from routes.auth import auth_bp
 from database.db import init_db
 
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 init_db(app)
 
 app.register_blueprint(prediction_bp)
+
+app.register_blueprint(auth_bp)
 if __name__ == "__main__":
     debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
     app.run(debug=debug_mode)
